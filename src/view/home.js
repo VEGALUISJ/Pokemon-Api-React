294e4ec2d9ff58020bd9a21825df5ea748fcdc3f
Jose Vega <luisvega.jlvv@gmail.com>
2020-05-26 15:34:04 -0400

    initial commit

---
+++ src/view/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../components/navbar'
+
+
+
+const Home = () => {
+    return (
+        <React.Fragment>
+            <Navbar />
+        </React.Fragment>
+    )
+}
+
+export default Home;
