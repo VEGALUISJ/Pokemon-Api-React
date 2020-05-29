a8cad412ee96c7e147ecc8871e8f24d67b7a84b7
Jose Vega <luisvega.jlvv@gmail.com>
2020-05-29 05:05:00 -0400

    landing img

--- src/view/home/home.jsx
+++ src/view/home/home.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import PokemonImg from "../../assets/pokemon-img.png"
 import './Home.css'
 
 const Home = () => {
     return (
-        <p>This is home component</p>
+        <div>
+            <div class="landingContent">
+                <div className="landingContent-text">
+                    <h3>Explore Pokemon-World like never before, with this WebApp.</h3>
+                    <p>With this Pokemon Api, you will be able to meet each detail of your favorite Pokemon.</p>
+                    <button type="button" class="btn btn-primary">Gotta catch 'em all</button>
+                </div>
+                <div className="landingContent-img">
+                    <img className="my-auto" src={ PokemonImg } height="360" alt="Pokemon"/>
+                </div>
+            </div>
+            <div class="DeckHolder">
+                <p>Here is where cards will be</p>
+            </div>
+        </div>
     )
 }
 
